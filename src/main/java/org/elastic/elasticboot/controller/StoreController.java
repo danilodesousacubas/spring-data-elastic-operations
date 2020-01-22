77a1b4eb6491aa78f47587c6fe15f65723fc2ce6
Danilo de Souza <danilo@hanzo.com.br>
2020-01-22 00:00:40 -0300

    update request param with store-name endpoint

--- src/main/java/org/elastic/elasticboot/controller/StoreController.java
+++ src/main/java/org/elastic/elasticboot/controller/StoreController.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,36 +22,35 @@ public class StoreController {
 
 	@Autowired
 	private StoreService storeService;
-		
+
 	@PostMapping
-	public Store create(@RequestBody Store store) {
+	public Store create(@RequestBody final Store store) {
 		Store storePersisted = storeService.save(store);
 		LOGGER.info(String.format("created store id: {id}", storePersisted.getId()));
 		return store;
 	}
-	
+
 	@GetMapping
-	public Page<Store> findAll(Pageable pageable){
+	public Page<Store> findAll(Pageable pageable) {
 		LOGGER.info("find all stores");
 		return storeService.findAll(pageable);
 	}
-	
+
 	@GetMapping("/count")
-	public long count(Pageable pageable){
+	public long count(Pageable pageable) {
 		LOGGER.info("get count stores");
 		return storeService.count();
 	}
-	
-	@GetMapping("/query?{}")
-	public Page<Store> findByStoreName(Pageable pageable, @RequestParam String storeName){
-		LOGGER.info(String.format("store name [%s]", storeName));
-		
+
+	@GetMapping("/query?{store-name}")
+	public Page<Store> findByStoreName(Pageable pageable, @RequestParam final String storeName) {
+		LOGGER.info(String.format("find store by name: [%s]", storeName));
 		return storeService.findByStoreNameUsingCustomQuery(storeName, pageable);
 	}
-	
+
 	@DeleteMapping("/operation/delete")
 	public void delete() {
-		LOGGER.info("delete all documents");
+		LOGGER.info("elastic operation for delete index");
 		storeService.delete();
 	}
 }
