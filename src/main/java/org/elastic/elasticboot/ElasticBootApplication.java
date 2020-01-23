3341e1dc24daec604e0d40c3db84ebfd056317cc
Danilo de Souza <danilo@hanzo.com.br>
2020-01-22 23:30:26 -0300

    Logs to start

--- src/main/java/org/elastic/elasticboot/ElasticBootApplication.java
+++ src/main/java/org/elastic/elasticboot/ElasticBootApplication.java
@@ -1,12 +1,16 @@
 package org.elastic.elasticboot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ElasticBootApplication {
-
+	private static final Logger LOGGER = LoggerFactory.getLogger(ElasticBootApplication.class);
+	
 	public static void main(String[] args) {
+		LOGGER.info("starting elastic boot application");
 		SpringApplication.run(ElasticBootApplication.class, args);
 	}
 	
