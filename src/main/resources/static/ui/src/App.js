336c5965fbd115b5081aba2eefdd4143c2b9f20c
Danilo de Souza <danilo@hanzo.com.br>
2020-01-25 20:13:34 -0300

    Created react component table

--- src/main/resources/static/ui/src/App.js
+++ src/main/resources/static/ui/src/App.js
@@ -1,13 +1,25 @@
 import React, { Fragment, Component } from 'react';
+import axios from 'axios';
 import Table from './Table';
 
 class App extends Component {
+    state = { stores: [] }
+    
+    componentDidMount() {
+        axios
+            .get("http://localhost:8080/store")
+            .then(res => {
+                const stores = res.data.content;
+                this.setState({ stores });
+        })
+        .catch(error => console.log("catch error: ", error));
+    }    
+
     render() {
         return (
             <Fragment>
                 <div className='container'>
-
-                    <Table />
+                    <Table stores={this.state.stores} />
                 </div>
             </Fragment>
         );
--- src/main/resources/static/ui/src/Table.js
+++ src/main/resources/static/ui/src/Table.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
 
 class Table extends Component {
-    
-    state = {
-        
-    }
-    
     render() {
+        const  {stores} = this.props;
         return (
-            <h1>
-                Store
-            </h1>
+           <TableStores stores={stores} />
         );
     }
 }
-    
+
+const TableStores = props => {
+    const lines = props.stores.map((l,i) => {
+         return(
+            <tr key={i}>
+                 <td>{l.id}</td>
+                 <td>{l.name}</td>
+             </tr>
+         );
+    });
+
+    return(
+        <table>
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>name</th>
+                </tr>
+            </thead>
+            <tbody>
+                {lines}
+            </tbody>
+        </table>
+    );
+}
+   
 export default Table;
